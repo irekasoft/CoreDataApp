f8907eac0ba832f96c9eed86adf17ead5104e52c
Muhammad Hijazi <irekasoft@gmail.com>
2016-11-06 21:51:37 +0800

    Initial Commit

---
+++ CoreDataApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Hijazi on 6/11/16.
+//  Copyright © 2016 iReka Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
